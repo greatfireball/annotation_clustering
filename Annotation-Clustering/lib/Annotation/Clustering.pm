138a9faedfac0eaa5123e3f7dc941c5bdb678f07
Frank Förster <foersterfrank@gmx.de>
2015-04-29 23:40:05 +0200

    Updated documentation

--- Annotation-Clustering/lib/Annotation/Clustering.pm
+++ Annotation-Clustering/lib/Annotation/Clustering.pm
@@ -8,48 +8,76 @@ our $VERSION = '0.2';
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
+
+=pod
 
 =head1 NAME
 
-Annotation::Clustering - Perl extension for blah blah blah
+Annotation::Clustering - Perl extension for clustering of annotation information
 
 =head1 SYNOPSIS
 
   use Annotation::Clustering;
-  blah blah blah
+  $VAR1 = = {
+          '423' => {
+                     'IucA_IucC' => '244.1'
+                   },
+          '420' => {
+                     'IucA_IucC' => '246.4',
+                     'YYY_ZZZ' => 'AAA'
+                   },
+          '200' => {
+                     'IucA_IucC' => 'XXX'
+                   }
+        };
+
+  my $out = generate_cluster($VAR1)
+
+  foreach my $cluster (keys %{$out})
+  {
+     print "$cluster\n";
+  }
+
+  # 194-206
+  # 414-429
 
 =head1 DESCRIPTION
 
-Stub documentation for Annotation::Clustering, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
+I have been asked by Hannes Horn if there is an easy method to cluster
+results of an bacterial genome annotation. This is my implementation
+of a clustering algorithm.
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+This module is hosted on
+L<https://github.com/greatfireball/annotation_clustering>.
 
 =head1 AUTHOR
 
-Frank Foerster, E<lt>frf53jh@E<gt>
+Frank Förster, E<lt>frank.foerster@biozentrum.uni-wuerzburg.de<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2015 by Frank Foerster
+The MIT License (MIT)
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.18.2 or,
-at your option, any later version of Perl 5 you may have available.
+Copyright (c) 2015 Frank Förster
 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
 =cut
